b8f52b18fb0b428c8b561208c474a320b84e4abd
juleshonorario <153901269+juleshonorario@users.noreply.github.com>
2023-12-21 13:02:39 +0000

    tasksheet1-1-2

--- ArithmeticTask.java
+++ ArithmeticTask.java
@@ -5,16 +5,19 @@ public static void main (String[] args) {
     int result = 1 + 2; // result is now 3
     System.out.println(result);
 
-    result = result -1; // result is now 2
+    result -= 1; // result is now 2
     System.out.println(result);
 
-    result = result * 2; // result is now 4
+    result *= 2; // result is now 4
     System.out.println(result);
 
-    result = result + 6; // result is now 10
+    result /= 2; // result is now 2
     System.out.println(result);
 
-    result = result % 7; // result is now 3
+    result += 8; // result is now 10
+    System.out.println(result);
+
+    result %= 7; // result is now 3
     System.out.println(result);
 }
 }
